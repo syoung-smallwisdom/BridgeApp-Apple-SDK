b5de4a590be67488097fd412ad561d1d3fcf290f
syoung-smallwisdom <shannon.young@sagebase.org>
2018-05-08 23:48:33 -0700

    Stub out a basic root view controller and app delegate.

---
+++ BridgeApp/BridgeApp/iOS/SBAAppDelegate.swift
@@ -0,0 +1,210 @@
+//
+//  SBAAppDelegate.swift
+//  BridgeApp (iOS)
+//
+//  Copyright © 2016-2018 Sage Bionetworks. All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without modification,
+// are permitted provided that the following conditions are met:
+//
+// 1.  Redistributions of source code must retain the above copyright notice, this
+// list of conditions and the following disclaimer.
+//
+// 2.  Redistributions in binary form must reproduce the above copyright notice,
+// this list of conditions and the following disclaimer in the documentation and/or
+// other materials provided with the distribution.
+//
+// 3.  Neither the name of the copyright holder(s) nor the names of any contributors
+// may be used to endorse or promote products derived from this software without
+// specific prior written permission. No license is granted to the trademarks of
+// the copyright holders even if such marks are included in this software.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+// ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
+// FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+// DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+// SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+// CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+// OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+// OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+//
+
+import UIKit
+
+/// `SBAAppDelegate` is an optional class that can be used as the appDelegate for an application.
+open class SBAAppDelegate : UIResponder, UIApplicationDelegate, RSDAlertPresenter, SBBBridgeErrorUIDelegate {
+    
+    open var window: UIWindow?
+    
+    // MARK: UIApplicationDelegate
+    
+    open func application(_ application: UIApplication, willFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization before application launch.
+        
+        // Setup bridge.
+        SBABridgeConfiguration.shared.setupBridge(with: SBAFactory())
+        
+        // Set the tint color.
+        self.window?.tintColor = UIColor.primaryTintColor
+        
+        // Replace the launch root view controller with an SBARootViewController
+        // This allows transitioning between root view controllers while a lock screen
+        // or onboarding view controller is being presented modally.
+        self.window?.rootViewController = SBARootViewController(rootViewController: self.window?.rootViewController)
+        
+        return true
+    }
+    
+    open func applicationDidBecomeActive(_ application: UIApplication) {
+        // Make sure that the content view controller is not hiding content.
+        rootViewController?.contentHidden = false
+    }
+    
+
+    // ------------------------------------------------
+    // MARK: RootViewController management
+    // ------------------------------------------------
+    
+    /// The root view controller for this app. By default, this is setup in `willFinishLaunchingWithOptions`
+    /// as the key window root view controller. This container view controller allows presenting
+    /// onboarding flow and/or a passcode modally while transitioning the underlying view controller
+    /// for the appropriate app state.
+    open var rootViewController: SBARootViewController? {
+        return window?.rootViewController as? SBARootViewController
+    }
+    
+    /// Convenience method for transitioning to the given view controller as the main window
+    /// rootViewController.
+    /// - parameters:
+    ///     - viewController: View controller to transition to.
+    ///     - state: State of the app.
+    ///     - animated: Should the transition be animated?
+    open func transition(to viewController: UIViewController, state: SBARootViewController.State, animated: Bool) {
+        guard let window = self.window, rootViewController?.state != state else { return }
+        if let root = self.rootViewController {
+            root.set(viewController: viewController, state: state, animated: animated)
+        }
+        else {
+            if (animated) {
+                UIView.transition(with: window,
+                                  duration: 0.3,
+                                  options: .transitionCrossDissolve,
+                                  animations: {
+                                    window.rootViewController = viewController
+                },
+                                  completion: nil)
+            }
+            else {
+                window.rootViewController = viewController
+            }
+        }
+    }
+    
+    
+    // ------------------------------------------------
+    // MARK: RSDAlertPresenter
+    // ------------------------------------------------
+    
+    /// Convenience method for presenting a modal view controller.
+    open func presentModal(_ viewController: UIViewController, animated: Bool, completion: (() -> Void)?) {
+        guard let rootVC = self.window?.rootViewController else { return }
+        var topViewController: UIViewController = rootVC
+        while let presentedVC = topViewController.presentedViewController {
+            if presentedVC.modalPresentationStyle != .fullScreen {
+                presentedVC.dismiss(animated: false, completion: nil)
+                break
+            }
+            else {
+                topViewController = presentedVC
+            }
+        }
+        topViewController.present(viewController, animated: animated, completion: completion)
+    }
+    
+    
+    // ------------------------------------------------
+    // MARK: Catastrophic startup errors
+    // ------------------------------------------------
+    
+    private var catastrophicStartupError: Error?
+    
+    /// Catastrophic Errors are errors from which the system cannot recover. By default,
+    /// this will display a screen that blocks all activity. The user is then asked to
+    /// update their app.
+    ///
+    /// - parameter animated:  Should the transition be animated.
+    open func showCatastrophicStartupErrorViewController(animated: Bool) {
+        
+        guard self.rootViewController?.state != .catastrophicError else { return }
+        
+        // If we cannot open the catastrophic error view controller (for some reason)
+        // then this is a fatal error
+        guard let vc = SBACatastrophicErrorViewController.instantiateWithMessage(catastrophicErrorMessage) else {
+            fatalError(catastrophicErrorMessage)
+        }
+        
+        // Present the view controller
+        transition(to: vc, state: .catastrophicError, animated: animated)
+    }
+    
+    /// Is there a catastrophic error.
+    public final var hasCatastrophicError: Bool {
+        return (catastrophicStartupError != nil)
+    }
+    
+    /// Register a catastrophic error. Once launch is complete, this will trigger showing
+    /// the error.
+    public final func registerCatastrophicStartupError(_ error: Error) {
+        self.catastrophicStartupError = error
+    }
+    
+    /// The error message to display for a catastrophic error.
+    open var catastrophicErrorMessage: String {
+        return catastrophicStartupError?.localizedDescription ??
+            Localization.localizedString("CATASTROPHIC_FAILURE_MESSAGE")
+    }
+    
+    
+    // ------------------------------------------------
+    // MARK: SBBBridgeErrorUIDelegate
+    // ------------------------------------------------
+    
+    /// Default implementation for handling a user who is not consented (because consent has been revoked
+    /// by the server).
+    open func handleUserNotConsentedError(_ error: Error, sessionInfo: Any, networkManager: SBBNetworkManagerProtocol?) -> Bool {
+        // TODO: syoung 05/08/2018 Handle unconsented user.
+        return true
+    }
+    
+    /// Default implementation for handling an unsupported app version is to display a catastrophic error.
+    open func handleUnsupportedAppVersionError(_ error: Error, networkManager: SBBNetworkManagerProtocol?) -> Bool {
+        registerCatastrophicStartupError(error)
+        DispatchQueue.main.async {
+            if let _ = self.window?.rootViewController {
+                self.showCatastrophicStartupErrorViewController(animated: true)
+            }
+        }
+        return true
+    }
+    
+    
+    // ------------------------------------------------
+    // MARK: Lock orientation to portrait by default
+    // ------------------------------------------------
+    
+    /// The default orientation lock if not overridden by setting the `orientationLock` property.
+    open var defaultOrientationLock: UIInterfaceOrientationMask {
+        return .portrait
+    }
+    
+    /// The `orientationLock` property is used to override the default allowed orientations.
+    public var orientationLock: UIInterfaceOrientationMask?
+    
+    /// - returns: The `orientationLock` or the `defaultOrientationLock` if nil.
+    open func application(_ application: UIApplication, supportedInterfaceOrientationsFor window: UIWindow?) -> UIInterfaceOrientationMask {
+        return orientationLock ?? defaultOrientationLock
+    }
+}
+
---
+++ BridgeApp/BridgeApp/iOS/SBARootViewController.swift
@@ -0,0 +1,170 @@
+//
+//  SBARootViewController.swift
+//  BridgeApp (iOS)
+//
+//  Copyright © 2016-2018 Sage Bionetworks. All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without modification,
+// are permitted provided that the following conditions are met:
+//
+// 1.  Redistributions of source code must retain the above copyright notice, this
+// list of conditions and the following disclaimer.
+//
+// 2.  Redistributions in binary form must reproduce the above copyright notice,
+// this list of conditions and the following disclaimer in the documentation and/or
+// other materials provided with the distribution.
+//
+// 3.  Neither the name of the copyright holder(s) nor the names of any contributors
+// may be used to endorse or promote products derived from this software without
+// specific prior written permission. No license is granted to the trademarks of
+// the copyright holders even if such marks are included in this software.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+// ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
+// FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+// DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+// SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+// CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+// OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+// OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+//
+
+import UIKit
+
+/// `SBARootViewController` is a root view controller implementation that allows the current "root"
+/// to be transitioned while there is a modal view controller displayed on top of the root. For
+/// example, when displaying a passcode or during onboarding.
+open class SBARootViewController: UIViewController {
+    
+    /// The "state" of the application. This can be used to track what view controller is currently being
+    /// displayed.
+    public enum State : Equatable {
+        
+        /// Initial launch view.
+        case launch
+        
+        /// Main view showing activities, etc. for the signed in user.
+        case main
+        
+        /// Study overview (onboarding) for the user who is *not* signed in.
+        case onboarding
+        
+        /// Unrecoverable error state. Displayed when the app must be updated or has reached end-of-life.
+        case catastrophicError
+        
+        /// A custom state that is defined by a specific application.
+        case custom(String)
+        
+        /// The string for the custom root view controller state.
+        public var customState: String? {
+            if case .custom(let str) = self {
+                return str
+            } else {
+                return nil
+            }
+        }
+    }
+    
+    /// The current state of the app.
+    public private(set) var state: State = .launch
+    
+    /// Should the content of the app be hidden when in the background?
+    public var contentHidden = false {
+        didSet {
+            guard contentHidden != oldValue && isViewLoaded else { return }
+            self.childViewControllers.first?.view.isHidden = contentHidden
+        }
+    }
+    
+    /// The status bar style to use for the view controller currently being shown. This only works with
+    /// projects set with view-controller-based status bar style.
+    open var statusBarStyle: UIStatusBarStyle = .default {
+        didSet {
+            setNeedsStatusBarAppearanceUpdate()
+        }
+    }
+    
+    /// - returns: The `statusBarStyle` property value.
+    override open var preferredStatusBarStyle: UIStatusBarStyle {
+        return statusBarStyle
+    }
+    
+    public required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+    
+    public override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?) {
+        super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
+    }
+    
+    /// Initialize using an initial unloaded root view controller. This is used to track state during
+    /// launch.
+    public init(rootViewController: UIViewController?) {
+        super.init(nibName: nil, bundle: nil)
+        _unloadedRootViewController = rootViewController
+    }
+    
+    override open func viewDidLoad() {
+        super.viewDidLoad()
+
+        // If there is no root view controller already loaded then do so now.
+        if self.childViewControllers.first == nil {
+            let isBlankVC = (_unloadedRootViewController == nil)
+            let viewController = self.rootViewController
+            self.addChildViewController(viewController)
+            viewController.view.frame = self.view.bounds
+            viewController.view.isHidden = contentHidden
+            if isBlankVC {
+                viewController.view.backgroundColor = UIColor.white
+            }
+            self.view.addSubview(viewController.view)
+            viewController.didMove(toParentViewController: self)
+        }
+    }
+    
+    /// The root view controller for the application.
+    public var rootViewController: UIViewController {
+        return self.childViewControllers.first ?? {
+            if _unloadedRootViewController == nil {
+                _unloadedRootViewController = UIViewController()
+            }
+            return _unloadedRootViewController!
+        }()
+    }
+    private var _unloadedRootViewController: UIViewController?
+    
+    /// The animation duration when transitioning between view controllers.
+    public var animationDuration: TimeInterval = 0.3
+    
+    /// The animation options when transitioning between view controllers.
+    public var animationOptions: UIViewAnimationOptions = [.transitionCrossDissolve]
+    
+    /// Set the new root view controller. This will use a crossfade animation to transition between the
+    /// two root view controllers.
+    public func set(viewController: UIViewController, state: State, animated: Bool) {
+        
+        guard isViewLoaded else {
+            _unloadedRootViewController = viewController
+            self.state = state
+            return
+        }
+        
+        // Set up state for view controllers.
+        self.rootViewController.willMove(toParentViewController: nil)
+        self.addChildViewController(viewController)
+        self.state = state
+        
+        // Set up new view initial alpha and frame.
+        viewController.view.frame = self.view.bounds
+        
+        let duration = animated ? animationDuration : 0.0
+        self.transition(from: self.rootViewController, to: viewController,
+                        duration: duration, options: animationOptions,
+                        animations: {}) { (finished) in
+                            self.rootViewController.removeFromParentViewController()
+                            viewController.didMove(toParentViewController: self)
+        }
+    }
+}
